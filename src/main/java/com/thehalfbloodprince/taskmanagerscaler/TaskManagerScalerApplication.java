8e23a228e1c047eba087b1aee557fc9d83523530
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-12-27 02:38:02 +0530

    Init

---
+++ src/main/java/com/thehalfbloodprince/taskmanagerscaler/TaskManagerScalerApplication.java
@@ -0,0 +1,13 @@
+package com.thehalfbloodprince.taskmanagerscaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerScalerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerScalerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thehalfbloodprince/taskmanagerscaler/TaskManagerScalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehalfbloodprince.taskmanagerscaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerScalerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
